0574e5b4c553f424d0dba4288d74335ca47f8931
Trump <Trump@gmail.com>
2024-05-08 23:54:38 +0800

    监听多个数据，第一个参数用数组

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="person">
-        <h1>情况四：监视【reactive】或者【ref】定义的【对象类型】数据中的【某个属性】</h1>
+        <h1>情况五：监视多个数据</h1>
         <h2>姓名：{{ person.name }}</h2>
         <h2>年龄：{{ person.age }}</h2>
         <h2>汽车：{{ person.car.c1 }}、{{ person.car.c2 }}</h2>
@@ -43,18 +43,10 @@ function changeCar() {
     }
 }
 
-// 监视响应式对象的某个属性，可以监听函数，所以将要监听的属性值作为一个函数的返回值来监听
-/**
- * 若该属性不是对象类型，需要写成函数类型
- * 若该属性是对象类型，可以直接对象名.属性，不过建议写成函数
- */
-// () => { return person.name }
-watch(() => person.name, (newValue, oldValue) => {
-    console.log("person.name属性发生变化", newValue, oldValue);
-})
-// 监视响应式对象的某个属性，并且该属性是个对象类型的，建议写成函数式，如果直接写person.car，不能监视到整个车改变
-watch(() => person.car, (newValue, oldValue) => {
-    console.log("person.car属性发生变化", newValue, oldValue);
+// 监视多个数据：名字和车
+// 也可以单独监视第一台车() => person.car.c1
+watch([() => person.name, () => person.car], (newValue, oldValue) => {
+    console.log("person.name或者person.car发生变化", newValue, oldValue);
 }, { deep: true })
 </script>
 
