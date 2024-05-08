fb564261126756a5a286699f2f771e911f3a89d9
Trump <Trump@gmail.com>
2024-05-08 21:29:08 +0800

    watch 基本类型数据 & stopWatch 监视和解除监视

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,39 +1,28 @@
 <template>
     <div class="person">
-        姓：<input type="text" v-model="firstName"><br>
-        名：<input type="text" v-model="lastName"><br>
-        <button @click="changeFullName">将全名改为li-si</button><br>
-        姓名：<span>{{ fullName }}</span><br>
-        姓名：<span>{{ fullName }}</span><br>
-        姓名：<span>{{ fullName }}</span><br>
-        姓名：<span>{{ fullName }}</span><br>
+        <h1>情况一：监视【ref】定义的【基本类型】数据</h1>
+        <h2>当前值：{{ sum }}</h2>
+        <button @click="changeSum">当前值+1</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { ref, computed } from 'vue';
+import { ref, watch } from 'vue';
 
-let firstName = ref('zhang')
-let lastName = ref('san')
-// 这样定义的fullName是一个计算属性，而且是只读的
-// let fullName = computed(() => {
-//     return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
-// })
-// 这样定义的fullName是一个计算属性，而且可读可写
-let fullName = computed({
-    get() {
-        return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
-    },
-    set(val) {
-        const [str1, str2] = val.split('-')
-        firstName.value = str1
-        lastName.value = str2
+let sum = ref(0)
+
+function changeSum() {
+    sum.value += 1
+}
+
+// 监视，当值变化时执行回调函数
+const stopWatch = watch(sum, (newValue, oldValue) => {
+    console.log('sum的值变化了', newValue, oldValue)
+    // 当newValue≥10时解除监视
+    if (newValue >= 10) {
+        stopWatch()
     }
 })
-
-function changeFullName() {
-    fullName.value = 'li-si'
-}
 </script>
 
 <style scoped>
