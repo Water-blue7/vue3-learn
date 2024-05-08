644b7c6393374bcbb7eea544e4b6996f9f909456
Trump <Trump@gmail.com>
2024-05-08 17:28:27 +0800

    vite-plugin-vue-setup-extend <script lang=ts setup name=自定义名字>

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,46 +1,27 @@
 <template>
     <div class="person">
-        <h2>姓名：{{ name }}</h2>
-        <h2>年龄：{{ age }}</h2>
-        <button @click="changeName">修改名字</button>
-        <button @click="changeAge">修改年龄</button>
-        <button @click="showTel">查看联系方式</button>
+        <h2>{{ a }}</h2>
     </div>
 </template>
 
-<script lang="ts">
+<!-- <script lang="ts">
 export default {
     name: "Person", //组件名
-    // 组合式API使用方法
-    setup() {
-        // console.log(this) // vue3中已经弱化this了
-
-        // 数据
-        let name = '张三' // 此时的name不是响应式的
-        let age = 19 // 此时的age不是响应式的
-        let tel = '13333333' // 此时的tel不是响应式的
-
-        // 方法
-        function changeName() {
-            console.log('changeName')
-            name = 'zhang-san' // 确实修改了name的值，但不是响应式的，页面没有变化
-            console.log(name)
-        }
-        function changeAge() {
-            age += 1
-        }
-        function showTel() {
-            alert(tel)
-        }
-
-        // return { name: name, age: age }
-        return { name, age, changeName, changeAge, showTel }
-    }
 };
+</script> -->
+
+<script lang="ts" setup name="Person2">
+let a = 12311;
 </script>
 
-
 <style scoped>
+.person {
+    background-color: #7a0505;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
 button {
     margin: 0 5px;
 }
