35718da31105cffc6454ab5e436f2309de2a4eee
Trump <Trump@gmail.com>
2024-05-09 00:21:15 +0800

    watchEffect:自动监视，不用写需要监视的属性

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,53 +1,40 @@
 <template>
     <div class="person">
-        <h1>情况五：监视多个数据</h1>
-        <h2>姓名：{{ person.name }}</h2>
-        <h2>年龄：{{ person.age }}</h2>
-        <h2>汽车：{{ person.car.c1 }}、{{ person.car.c2 }}</h2>
-        <button @click="changeName">修改名字</button>
-        <button @click="changeAge">修改年龄</button>
-        <button @click="changeC1">修改第一台车</button>
-        <button @click="changeC2">修改第二台车</button>
-        <button @click="changeCar">修改所有车</button>
+        <h2>水温达到60℃或水位达到80cm时向服务器发送请求</h2>
+        <h2>当前水温：{{ temp }}℃</h2>
+        <h2>当前水位：{{ height }}cm</h2>
+        <button @click="changeTemp">点我水温+10℃</button>
+        <button @click="changeHeight">点我水位+10cm</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
-import { reactive, watch } from 'vue';
+import { ref, watch, watchEffect } from 'vue';
 
-// reactive定义的对象不可整体修改
-let person = reactive({
-    name: 'zhang-san',
-    age: 18,
-    car: {
-        c1: '奔驰',
-        c2: '宝马'
+let temp = ref(10)
+let height = ref(0)
+function changeTemp() {
+    temp.value += 10
+}
+function changeHeight() {
+    height.value += 10
+}
+
+// 监视
+// watch([temp, height], (value) => {
+//     let [newTemp, newHeight] = value
+//     console.log(newTemp, newHeight);
+//     if (newTemp >= 60 || newHeight >= 80) {
+//         console.log('向服务器发起一个请求');
+//     }
+// })
+
+// watchEffect：不用告诉它要监视什么属性
+watchEffect(() => {
+    if (temp.value >= 60 || height.value >= 80) {
+        console.log('向服务器发起一个请求');
     }
 })
-function changeName() {
-    person.name += '~'
-}
-function changeAge() {
-    person.age += 1
-}
-function changeC1() {
-    person.car.c1 = 'BYD'
-}
-function changeC2() {
-    person.car.c2 = '法拉利'
-}
-function changeCar() {
-    person.car = {
-        c1: 'qq',
-        c2: '五菱'
-    }
-}
-
-// 监视多个数据：名字和车
-// 也可以单独监视第一台车() => person.car.c1
-watch([() => person.name, () => person.car], (newValue, oldValue) => {
-    console.log("person.name或者person.car发生变化", newValue, oldValue);
-}, { deep: true })
 </script>
 
 <style scoped>
