9c1a2db0c87f717a5e72aa02f7e04b23d4cb6248
Trump <Trump@gmail.com>
2024-05-08 16:16:38 +0800

    add Person.vue

---
+++ hello_vue3/src/components/Person.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <!-- <h2>电话：{{ tel }}</h2> -->
+        <button @click="showTel">查看联系方式</button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: "Person", //组件名
+    data() {
+        return {
+            name: "张三",
+            age: 19,
+            tel: "133333333",
+        };
+    },
+    methods: {
+        changeName() {
+            this.name = "zhangsan";
+        },
+        changeAge() {
+            this.age += 1;
+        },
+        showTel() {
+            alert(this.tel);
+        },
+    },
+};
+</script>
+
+
+<style scoped>
+.person {
+    background-color: #bc2626;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+}
+
+button {
+    margin: 0 5px;
+}
+</style>
