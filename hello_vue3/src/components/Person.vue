bf8d8d788d35d140bc027b1d1aee1097cd07aa8f
Trump <Trump@gmail.com>
2024-05-08 18:06:10 +0800

    响应式 ref 基本类型数据

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,17 +1,36 @@
 <template>
     <div class="person">
-        <h2>{{ a }}</h2>
+        <h2>姓名：{{ name }}</h2>
+        <h2>年龄：{{ age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
     </div>
 </template>
 
-<!-- <script lang="ts">
-export default {
-    name: "Person", //组件名
-};
-</script> -->
+<script lang="ts" setup name="Person">
+// 响应式
+import { ref } from 'vue'
 
-<script lang="ts" setup name="Person2">
-let a = 12311;
+// 数据
+let name = ref('张三')
+let age = ref(19)
+let tel = '13333333'
+
+// 方法
+function changeName() {
+    console.log('changeName')
+    name.value = 'zhang-san' // 确实修改了name的值，但不是响应式的，页面没有变化
+    console.log(name.value)
+}
+function changeAge() {
+    age.value += 1
+}
+function showTel() {
+    alert(tel)
+}
+
+// 不用写return了
 </script>
 
 <style scoped>
