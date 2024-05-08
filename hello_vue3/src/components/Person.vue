2f28f506534dc24f74e4a0452fa892078b64e5e6
Trump <Trump@gmail.com>
2024-05-08 19:31:36 +0800

    toRefs 将reactive定义的对象中的每一组k:y转成ref

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,32 +1,33 @@
 <template>
     <div class="person">
-        <h2>汽车信息：一辆{{ car.brand }}车，价值{{ car.price }}万</h2>
-        <button @click="changePrice">修改汽车价格</button>
-        <button @click="changeCar">修改汽车</button>
-        <br>
+        <h2>姓名：{{ person.name }}</h2>
+        <h2>年龄：{{ person.age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
-// 用ref实现响应式对象类型数据，底层用的是reactive
-import { ref, reactive } from 'vue';
+// toRefs:把reactive定义的对象中的每一组k:y转成ref
+import { reactive, toRefs } from 'vue'
 
-// 数据
-let car = reactive({
-    brand: '奔驰',
-    price: 100
+let person = reactive({
+    name: '张三',
+    age: 15
 })
 
-// 方法
-function changePrice() {
-    car.price += 10
+// 解构赋值
+// toRefs将对象中每一组的ky转成ref
+let { name, age } = toRefs(person)
+
+function changeName() {
+    name.value += '~'
+    console.log(person.name, name.value);
 }
-function changeCar() {
-    // 用赋值的方式，页面无任何变化
-    // 使用Object的assign方法
-    Object.assign(car, { brand: '宝马', price: 200 })
+function changeAge() {
+    age.value += 1
+    console.log(person.age, age.value);
 }
-// 不用写return了
 </script>
 
 <style scoped>
