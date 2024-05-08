560513d4ee3f5cabaed01a73dee76db308da1177
Trump <Trump@gmail.com>
2024-05-08 16:44:50 +0800

    Vue3 setup 组合式API

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="app">
-    <h1>你好啊</h1>
-    <Person />
-  </div>
+  <Person />
 </template>
 
 <script lang="ts">
--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -4,7 +4,6 @@
         <h2>年龄：{{ age }}</h2>
         <button @click="changeName">修改名字</button>
         <button @click="changeAge">修改年龄</button>
-        <!-- <h2>电话：{{ tel }}</h2> -->
         <button @click="showTel">查看联系方式</button>
     </div>
 </template>
@@ -12,36 +11,36 @@
 <script lang="ts">
 export default {
     name: "Person", //组件名
-    data() {
-        return {
-            name: "张三",
-            age: 19,
-            tel: "133333333",
-        };
-    },
-    methods: {
-        changeName() {
-            this.name = "zhangsan";
-        },
-        changeAge() {
-            this.age += 1;
-        },
-        showTel() {
-            alert(this.tel);
-        },
-    },
+    // 组合式API使用方法
+    setup() {
+        // console.log(this) // vue3中已经弱化this了
+
+        // 数据
+        let name = '张三' // 此时的name不是响应式的
+        let age = 19 // 此时的age不是响应式的
+        let tel = '13333333' // 此时的tel不是响应式的
+
+        // 方法
+        function changeName() {
+            console.log('changeName')
+            name = 'zhang-san' // 确实修改了name的值，但不是响应式的，页面没有变化
+            console.log(name)
+        }
+        function changeAge() {
+            age += 1
+        }
+        function showTel() {
+            alert(tel)
+        }
+
+        // return { name: name, age: age }
+        return { name, age, changeName, changeAge, showTel }
+    }
 };
 </script>
 
 
 <style scoped>
-.person {
-    background-color: #bc2626;
-    box-shadow: 0 0 10px;
-    border-radius: 10px;
-    padding: 20px;
-}
-
 button {
     margin: 0 5px;
 }
