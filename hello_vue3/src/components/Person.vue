109f8c01ec9f03fb12d9f879bf080080c220316d
Trump <Trump@gmail.com>
2024-05-08 21:00:27 +0800

    computed 计算属性，只读，可读可写

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,32 +1,38 @@
 <template>
     <div class="person">
-        <h2>姓名：{{ person.name }}</h2>
-        <h2>年龄：{{ person.age }}</h2>
-        <button @click="changeName">修改名字</button>
-        <button @click="changeAge">修改年龄</button>
+        姓：<input type="text" v-model="firstName"><br>
+        名：<input type="text" v-model="lastName"><br>
+        <button @click="changeFullName">将全名改为li-si</button><br>
+        姓名：<span>{{ fullName }}</span><br>
+        姓名：<span>{{ fullName }}</span><br>
+        姓名：<span>{{ fullName }}</span><br>
+        姓名：<span>{{ fullName }}</span><br>
     </div>
 </template>
 
 <script lang="ts" setup name="Person">
-// toRefs:把reactive定义的对象中的每一组k:y转成ref
-import { reactive, toRefs } from 'vue'
+import { ref, computed } from 'vue';
 
-let person = reactive({
-    name: '张三',
-    age: 15
+let firstName = ref('zhang')
+let lastName = ref('san')
+// 这样定义的fullName是一个计算属性，而且是只读的
+// let fullName = computed(() => {
+//     return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+// })
+// 这样定义的fullName是一个计算属性，而且可读可写
+let fullName = computed({
+    get() {
+        return firstName.value.slice(0, 1).toUpperCase() + firstName.value.slice(1) + '-' + lastName.value
+    },
+    set(val) {
+        const [str1, str2] = val.split('-')
+        firstName.value = str1
+        lastName.value = str2
+    }
 })
 
-// 解构赋值
-// toRefs将对象中每一组的ky转成ref
-let { name, age } = toRefs(person)
-
-function changeName() {
-    name.value += '~'
-    console.log(person.name, name.value);
-}
-function changeAge() {
-    age.value += 1
-    console.log(person.age, age.value);
+function changeFullName() {
+    fullName.value = 'li-si'
 }
 </script>
 
