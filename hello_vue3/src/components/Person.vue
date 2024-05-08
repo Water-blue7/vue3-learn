c2034d0303b910523c7b010cdda2e636dff14af2
Trump <Trump@gmail.com>
2024-05-08 22:51:13 +0800

    监听对象类型数据中的某个属性：写成函数式，如果该属性是个对象类型，需要手动开启深度监视

--- hello_vue3/src/components/Person.vue
+++ hello_vue3/src/components/Person.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="person">
-        <h1>情况三：监视【reactive】定义的【对象类型】数据</h1>
+        <h1>情况四：监视【reactive】或者【ref】定义的【对象类型】数据中的【某个属性】</h1>
         <h2>姓名：{{ person.name }}</h2>
         <h2>年龄：{{ person.age }}</h2>
+        <h2>汽车：{{ person.car.c1 }}、{{ person.car.c2 }}</h2>
         <button @click="changeName">修改名字</button>
         <button @click="changeAge">修改年龄</button>
-        <button @click="changePerson">修改整个人</button>
+        <button @click="changeC1">修改第一台车</button>
+        <button @click="changeC2">修改第二台车</button>
+        <button @click="changeCar">修改所有车</button>
     </div>
 </template>
 
@@ -15,28 +18,44 @@ import { reactive, watch } from 'vue';
 // reactive定义的对象不可整体修改
 let person = reactive({
     name: 'zhang-san',
-    age: 18
+    age: 18,
+    car: {
+        c1: '奔驰',
+        c2: '宝马'
+    }
 })
-
 function changeName() {
     person.name += '~'
 }
 function changeAge() {
     person.age += 1
 }
-function changePerson() {
-    Object.assign(person, { name: 'li-si', age: 20 })
+function changeC1() {
+    person.car.c1 = 'BYD'
+}
+function changeC2() {
+    person.car.c2 = '法拉利'
+}
+function changeCar() {
+    person.car = {
+        c1: 'qq',
+        c2: '五菱'
+    }
 }
 
-/*
-监视：情况三：监视【reactive】定义的【对象类型】数据
-深度监视：默认开启
-*/
-const stopWatch = watch(person, (newValue, oldValue) => {
-    console.log('person变化了', newValue, oldValue)
-}, {
-    deep: true
+// 监视响应式对象的某个属性，可以监听函数，所以将要监听的属性值作为一个函数的返回值来监听
+/**
+ * 若该属性不是对象类型，需要写成函数类型
+ * 若该属性是对象类型，可以直接对象名.属性，不过建议写成函数
+ */
+// () => { return person.name }
+watch(() => person.name, (newValue, oldValue) => {
+    console.log("person.name属性发生变化", newValue, oldValue);
 })
+// 监视响应式对象的某个属性，并且该属性是个对象类型的，建议写成函数式，如果直接写person.car，不能监视到整个车改变
+watch(() => person.car, (newValue, oldValue) => {
+    console.log("person.car属性发生变化", newValue, oldValue);
+}, { deep: true })
 </script>
 
 <style scoped>
