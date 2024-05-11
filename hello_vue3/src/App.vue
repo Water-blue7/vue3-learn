ac43670e5c818932f3b32d989270a882cf1a5a9f
Trump <Trump@gmail.com>
2024-05-12 03:52:37 +0800

    写了一些样式，清空客户端浏览器的localStorage

--- hello_vue3/src/App.vue
+++ hello_vue3/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Count />
+    <!-- <Count /> -->
     <br>
     <LoveTalk />
   </div>
--- hello_vue3/src/components/LoveTalk.vue
+++ hello_vue3/src/components/LoveTalk.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="talk">
-    <button @click="getLoveTalk">获取一句土味情话</button>
+    <button class="btn1" @click="getLoveTalk">获取一句土味情话</button>
+    <button class="btn2" @click="clearLocalStorage">清空</button>
     <ul>
       <li v-for="talk in talkList" :key="talk.id">{{ talk.title }}</li>
     </ul>
@@ -23,7 +24,7 @@ const { talkList } = storeToRefs(loveTalkStore)
  */
 loveTalkStore.$subscribe((mutate, state) => {
   // 获取新的土味情话后控制台打印了这句话，有点像watch
-  console.log('loveTalkStore里面保存的数据发生了变化',mutate, state);
+  console.log('loveTalkStore里面保存的数据发生了变化', mutate, state);
   // 将数据存到浏览器的localStorage中，key-value形式
   localStorage.setItem('talkList', JSON.stringify(state.talkList))
 })
@@ -31,6 +32,11 @@ loveTalkStore.$subscribe((mutate, state) => {
 // 方法
 async function getLoveTalk() {
   loveTalkStore.getATalk()
+}
+
+function clearLocalStorage() {
+  localStorage.clear();
+  talkList.value = [];
 }
 </script>
 
@@ -40,5 +46,68 @@ async function getLoveTalk() {
   padding: 10px;
   border-radius: 10px;
   box-shadow: 0 0 10px;
+  font-size: 20px;
+  text-align: center;
+  /* 将容器内的内容水平居中 */
+}
+
+.btn1 {
+  /* 设置按钮的左右外边距为自动，使其水平居中 */
+  margin: 0 auto;
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #3498db;
+  color: #fff;
+  border: none;
+  border-radius: 30px;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  font-size: 20px;
+}
+
+.btn1:hover {
+  background-color: #2980b9;
+}
+
+.btn2 {
+  /* 设置按钮的左右外边距为自动，使其水平居中 */
+  margin: 0 auto;
+  display: inline-block;
+  padding: 10px 20px;
+  background-color: #c62424;
+  color: #fff;
+  border: none;
+  border-radius: 30px;
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  transition: background-color 0.3s;
+  font-size: 20px;
+  margin-left: 30px;
+}
+
+.btn2:hover {
+  background-color: #a30404;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  padding: 20px;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  margin-bottom: 10px;
+  transition: box-shadow 0.3s;
+}
+
+li:hover {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 </style>
