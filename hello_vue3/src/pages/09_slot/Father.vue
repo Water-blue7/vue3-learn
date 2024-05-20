e3443e0d68a1be8d3f82298e7917d5b74744a5a3
Trump <Trump@gmail.com>
2024-05-21 00:00:21 +0800

    插槽_作用域插槽 子组件维护自己所有的数据结构，交互等等，但是呈现出来的结构由父组件决定

--- hello_vue3/src/pages/09_slot/Father.vue
+++ hello_vue3/src/pages/09_slot/Father.vue
@@ -2,52 +2,41 @@
   <div class="father">
     <h3>父组件</h3>
     <div class="content">
-      <Category>
-        <template v-slot:s2>
+      <Game>
+        <!-- 作用域插槽接受数据：v-slot="params" -->
+        <template v-slot="params">
           <ul>
-            <li v-for="g in games" :key="g.id">{{ g.name }}</li>
+            <li v-for="y in params.youxi" :key="y.id">
+              {{ y.name }}
+            </li>
           </ul>
         </template>
-        <template v-slot:s1>
-          <h2>热门游戏列表</h2>
-        </template>
-      </Category>
+      </Game>
 
-      <Category>
-        <template v-slot:s2>
-          <img :src="imgUrl" alt="">
+      <Game>
+        <template v-slot="params">
+          <ol>
+            <li v-for="item in params.youxi" :key="item.id">
+              {{ item.name }}
+            </li>
+          </ol>
         </template>
-        <template v-slot:s1>
-          <h2>今日美食城市</h2>
-        </template>
-      </Category>
+      </Game>
 
-      <!-- v-slot:s2简写#s2 -->
-      <Category>
-        <template #s2>
-          <video video :src="videoUrl" controls></video>
+      <Game>
+        <!-- 每次都params.比较麻烦，也可以解构 -->
+        <!-- 作用域插槽如果没有名字：v-slot="params"  或者  #default="{youxi}" 如果有名字v-slot:name="params"-->
+        <template #default="{youxi}">
+          <h3 v-for="g in youxi" :key="g.id">{{ g.name }}</h3>
         </template>
-        <template #s1>
-          <h2>今日影视推荐</h2>
-        </template>
-      </Category>
+      </Game>
+
     </div>
   </div>
 </template>
 
 <script setup lang="ts" name="Father">
-  import Category from './Category.vue'
-  import { ref,reactive } from "vue";
-
-  let games = reactive([
-    {id:'asgytdfats01',name:'英雄联盟'},
-    {id:'asgytdfats02',name:'王者农药'},
-    {id:'asgytdfats03',name:'红色警戒'},
-    {id:'asgytdfats04',name:'斗罗大陆'}
-  ])
-  let imgUrl = ref('https://z1.ax1x.com/2023/11/19/piNxLo4.jpg')
-  let videoUrl = ref('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4')
-
+  import Game from './Game.vue'
 </script>
 
 <style scoped>
@@ -57,18 +46,10 @@
     border-radius: 10px;
   }
   .content {
-    /* 将content的直接子元素布局方式：子元素会按照指定的 flex 方向（默认是水平，即从左到右）排列，并且可以灵活地调整子元素的大小和位置。 */
     display: flex;
-    /* 设置子元素在主轴（默认是水平轴）方向上的对齐方式：space-evenly 会使得子元素均匀分布，每个子元素之间的间距，以及第一个子元素和容器左边缘之间的间距，最后一个子元素和容器右边缘之间的间距都相等。 */
     justify-content: space-evenly;
   }
   img,video {
     width: 100%;
-  }
-  h2 {
-    background-color: orange;
-    text-align: center;
-    font-size: 20px;
-    font-weight: 800;
   }
 </style>
