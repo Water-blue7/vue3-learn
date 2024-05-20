52f7a517d849fa8138841736bb0fba68442f98de
Trump <Trump@gmail.com>
2024-05-20 23:09:03 +0800

    插槽_默认插槽 1、双标签 2、<slot>显示双标签中的内容</slot>

---
+++ hello_vue3/src/pages/09_slot/Category.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="category">
+    <h2>{{title}}</h2>
+    <!-- 将父组件中双标签中的内容显示出来，如果双标签中没有任何东西就显示默认内容 -->
+    <slot>默认内容</slot>
+  </div>
+</template>
+
+<script setup lang="ts" name="Category">
+  defineProps(['title'])
+</script>
+
+<style scoped>
+  .category {
+    background-color: skyblue;
+    border-radius: 10px;
+    box-shadow: 0 0 10px;
+    padding: 10px;
+    width: 200px;
+    height: 300px;
+  }
+  h2 {
+    background-color: orange;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 800;
+  }
+</style>
---
+++ hello_vue3/src/pages/09_slot/Father.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="father">
+    <h3>父组件</h3>
+    <div class="content">
+      <!-- 这里写双标签，就可以 -->
+      <Category title="热门游戏列表">
+        <ul>
+          <li v-for="g in games" :key="g.id">{{ g.name }}</li>
+        </ul>
+      </Category>
+      <Category title="今日美食城市">
+        <img :src="imgUrl" alt="">
+      </Category>
+      <Category title="今日影视推荐">
+        <!-- video要加上controls，显示控制按钮 -->
+        <video :src="videoUrl" controls></video>
+      </Category>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" name="Father">
+  import Category from './Category.vue'
+  import { ref,reactive } from "vue";
+
+  let games = reactive([
+    {id:'asgytdfats01',name:'英雄联盟'},
+    {id:'asgytdfats02',name:'王者农药'},
+    {id:'asgytdfats03',name:'红色警戒'},
+    {id:'asgytdfats04',name:'斗罗大陆'}
+  ])
+  let imgUrl = ref('https://lh3.googleusercontent.com/a/ACg8ocJAK9rlS0BJk-MSuuvPRZerTLTGxwhy2Maxe-YOxbq_p2t49uDA')
+  let videoUrl = ref('http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4')
+
+</script>
+
+<style scoped>
+  .father {
+    background-color: rgb(165, 164, 164);
+    padding: 20px;
+    border-radius: 10px;
+  }
+  .content {
+    display: flex;
+    justify-content: space-evenly;
+  }
+  /* 并集选择器 */
+  img,video {
+    width: 100%;
+  }
+</style>
